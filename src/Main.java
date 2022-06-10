8078f364beee341e7ff0f75892a59c28185d67a9
Tom Fegan <tomfegan@Toms-MacBook-Air.local>
2022-06-10 09:25:35 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LeapYearCalculator.isLeapYear(-1600));
+        System.out.println(LeapYearCalculator.isLeapYear(1600));
+        System.out.println(LeapYearCalculator.isLeapYear(2017));
+        System.out.println(LeapYearCalculator.isLeapYear(2000));
+    }
+}
